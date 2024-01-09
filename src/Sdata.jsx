1c6f669a4c7dd85cfad8460d91b5fb4a1f05f079
mohd shadab <Mohd.S.mca2022@iujaipur.edu.in>
2024-01-09 18:08:39 +0530

    files add

---
+++ src/Sdata.jsx
@@ -0,0 +1,52 @@
+const Sdata = [
+        {
+            id:1,
+            sname : "Dark",
+            imgsrc : "https://wallpapercave.com/dwp1x/wp4995607.png",
+            title : "A Netflix Orignal Series",
+            link : "https://www.imdb.com/title/tt5753856/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_dark",
+        },
+
+        {
+            id:2,
+            sname : "jessica jones ",
+            imgsrc : "https://wallpapercave.com/dwp1x/wp1882405.jpg",
+            title : "A Netflix Orignal Series",
+            link : "https://www.imdb.com/title/tt2357547/?ref_=nv_sr_srsg_0_tt_1_nm_7_q_jessica%2520jones",
+        },
+        
+        {   
+            id:3,
+            sname : "Stranger Thing ",
+            imgsrc : "https://wallpapercave.com/dwp1x/wp5913975.jpg",
+            title : "A Netflix Orignal Series",
+            link : "https://www.imdb.com/title/tt9815454/ ",
+        },
+        {
+            id:4,
+            sname : "daredevil  ",
+            imgsrc : "https://wallpapercave.com/dwp1x/wp1917128.jpg",
+            title : "A Netflix Orignal Series",
+            link : "https://www.imdb.com/title/tt3322312/",
+        },
+
+        {   
+            id:5,
+            sname : "the witcher ",
+            imgsrc : "https://wallpapercave.com/dwp1x/wp5143620.jpg",
+            title : "A Netflix Orignal Series",
+            link : "https://www.imdb.com/title/tt5180504/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_the%2520witcher",
+        },
+
+        {
+            id:6,
+            sname : "you could save me ",
+            imgsrc : "https://wallpapercave.com/dwp1x/wp3049484.jpg",
+            title : "A Netflix Orignal Series",
+            link : "https://www.imdb.com/title/tt5180504/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_the%2520witcher",
+        }
+        
+
+]
+
+export default Sdata;
